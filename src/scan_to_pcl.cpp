a72dbaa646c6c1f41fca34b2378c8d6d54762ae7
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-04-01 03:27:17 +0900

    Update for ROS2 Humble

--- src/scan_to_pcl.cpp
+++ src/scan_to_pcl.cpp
@@ -1,34 +1,35 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+
+#include <rclcpp/rclcpp.hpp>
+
 #include "scan_to_pcl_ros/scan_to_pcl_ros.hpp"
 
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+using namespace std::chrono_literals;
 
-ScanToPcl::ScanToPcl():private_nh_("~")
+ScanToPcl::ScanToPcl() : Node("laserScan_to_pointcloud")
 {
-    private_nh_.param<int>("hz", hz_, 10);
-    private_nh_.param<std::string>("frame_id", frame_id_, "base_link");
-    hokuyo_sub_ = nh_.subscribe("/scan", 1, &ScanToPcl::hokuyo_callback, this);
-    pcl_from_scan_pub_ = nh_.advertise<PointCloud>("/pcl_from_scan", 1);
+    frame_id_ = this->declare_parameter<std::string>("frame_id", "base_link");
+
+    hokuyo_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "/scan", rclcpp::QoS(1).reliable(),
+        std::bind(&ScanToPcl::hokuyo_callback, this, std::placeholders::_1));
+    pcl_from_scan_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+        "/pcl_from_scan", rclcpp::QoS(1).reliable());
+
+    RCLCPP_INFO_STREAM(this->get_logger(), "=== " << rclcpp::Node::get_name() << " ===");
+    RCLCPP_INFO_STREAM(this->get_logger(), "frame_id: " << frame_id_);
 }
 
-void ScanToPcl::hokuyo_callback(const sensor_msgs::LaserScan::ConstPtr& scan_in)
+void ScanToPcl::hokuyo_callback(sensor_msgs::msg::LaserScan::ConstSharedPtr scan_msg)
 {
+    auto cloud_msg = std::make_unique<sensor_msgs::msg::PointCloud2>();
     laser_geometry::LaserProjection projector;
-    sensor_msgs::PointCloud2 cloud;
-    projector.projectLaser(*scan_in, cloud); // convert a sensor_msgs/LaserScan to a sensor_msgs/PointCloud
+    projector.projectLaser(*scan_msg, *cloud_msg); // convert a sensor_msgs/msg/LaserScan to a sensor_msgs/msg/PointCloud2
 
     // Publish the new point cloud.
-    cloud.header.frame_id = frame_id_;
-    cloud.header.stamp = scan_in->header.stamp;
-    pcl_from_scan_pub_.publish(cloud);
-}
-
-void ScanToPcl::process()
-{
-    ros::Rate loop_rate(hz_);
-
-    while(ros::ok())
-    {
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+    cloud_msg->header.frame_id = frame_id_;
+    cloud_msg->header.stamp = scan_msg->header.stamp;
+    pcl_from_scan_pub_->publish(std::move(cloud_msg));
 }
