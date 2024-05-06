2d7fcf839f23ba755c3d1e01a219ac319df215ff
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-05-06 09:31:02 +0900

    Remove header file (#6)
    * Update CMakeLists.txt
    
    * Remove header file

--- src/scan_to_pcl.cpp
+++ src/scan_to_pcl.cpp
@@ -5,37 +5,57 @@
  * @copyright Copyright (c) 2024
  */
 
+#include <laser_geometry/laser_geometry.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <string>
+#include <tf/transform_listener.h>
 
-#include "scan_to_pcl_ros/scan_to_pcl_ros.h"
-
-ScanToPcl::ScanToPcl() : private_nh_("~")
+class ScanToPcl
 {
-  private_nh_.param<int>("hz", hz_, 10);
-  private_nh_.param<std::string>("frame_id", frame_id_, "base_link");
-  hokuyo_sub_ = nh_.subscribe("/scan", 1, &ScanToPcl::hokuyo_callback, this);
-  pcl_from_scan_pub_ = nh_.advertise<pcl::PointCloud<pcl::PointXYZ>>("/pcl_from_scan", 1);
-}
-
-void ScanToPcl::hokuyo_callback(const sensor_msgs::LaserScan::ConstPtr &scan_in)
-{
-  laser_geometry::LaserProjection projector;
-  sensor_msgs::PointCloud2 cloud;
-  projector.projectLaser(*scan_in, cloud);  // convert a sensor_msgs/LaserScan to a sensor_msgs/PointCloud
-
-  // Publish the new point cloud.
-  cloud.header.frame_id = frame_id_;
-  cloud.header.stamp = scan_in->header.stamp;
-  pcl_from_scan_pub_.publish(cloud);
-}
-
-void ScanToPcl::process()
-{
-  ros::Rate loop_rate(hz_);
-
-  while (ros::ok())
+public:
+  ScanToPcl() : private_nh_("~")
   {
-    ros::spinOnce();
-    loop_rate.sleep();
+    private_nh_.param<std::string>("frame_id", frame_id_, std::string("base_scan"));
+
+    cloud_pub_ = nh_.advertise<pcl::PointCloud<pcl::PointXYZ>>("/cloud", 1);
+    scan_sub_ = nh_.subscribe("/scan", 1, &ScanToPcl::scan_callback, this);
+
+    tf_listener_.setExtrapolationLimit(ros::Duration(0.1));
+
+    ROS_INFO_STREAM(ros::this_node::getName() << " node has started..");
+    ROS_INFO_STREAM("frame_id: " << frame_id_);
   }
+
+private:
+  void scan_callback(const sensor_msgs::LaserScan::ConstPtr &msg)
+  {
+    laser_geometry::LaserProjection projector;
+    sensor_msgs::PointCloud2 cloud;
+    projector.transformLaserScanToPointCloud(frame_id_, *msg, cloud, tf_listener_);
+    cloud.header.frame_id = frame_id_;
+    cloud.header.stamp = ros::Time::now();
+    cloud_pub_.publish(cloud);
+  }
+
+  std::string frame_id_;
+
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+  ros::Publisher cloud_pub_;
+  ros::Subscriber scan_sub_;
+
+  tf::TransformListener tf_listener_;
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "scan_to_pcl");
+  ScanToPcl scan_to_pcl;
+  ros::spin();
+
+  return 0;
 }
